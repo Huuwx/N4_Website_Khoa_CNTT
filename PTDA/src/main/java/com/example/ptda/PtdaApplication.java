18b5cb466a793e263516d1f99ebbb03e7ad715b9
VuDangHuong <danghuong30082003@gmail.com>
2025-03-07 18:56:59 +0700

    bo cu code

---
+++ PTDA/src/main/java/com/example/ptda/PtdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ptda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtdaApplication.class, args);
+    }
+
+}
---
+++ PTDA/src/test/java/com/example/ptda/PtdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ptda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
