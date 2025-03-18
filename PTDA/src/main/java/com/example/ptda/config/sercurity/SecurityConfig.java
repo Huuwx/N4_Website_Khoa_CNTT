023cf04a272c8dda46ae51aa971eef3b5e47e525
Huuwx <chumanhhuu2@gmail.com>
2025-03-18 20:58:59 +0700

    update SecurityConfig

--- PTDA/src/main/java/com/example/ptda/config/sercurity/SecurityConfig.java
+++ PTDA/src/main/java/com/example/ptda/config/sercurity/SecurityConfig.java
@@ -2,8 +2,8 @@ package com.example.ptda.config.sercurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -13,6 +13,7 @@ import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
